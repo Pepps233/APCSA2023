ff801b7c183652fb9c44c41837c17f2c76e4d26e
pep233 <yibai14651@gmail.com>
2024-03-22 14:20:02 -0700

    binary searching months

--- src/main/java/org/APCSA2023/BinarySearch/MonthsHelper.java
+++ src/main/java/org/APCSA2023/BinarySearch/MonthsHelper.java
@@ -1,13 +1,42 @@
 package org.APCSA2023.BinarySearch;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 
 public class MonthsHelper {
-    public static int monthBinarySearch(ArrayList<String> months) {
-        return 9;
+    public static int monthBinarySearch(ArrayList<String> months, String target) {
+        int highest = months.size() -1;
+        int lowest = 0;
+        int middleIndex;
+        String middleString;
+        middleIndex = getMiddleIndex(lowest, highest);
+        middleString = months.get(middleIndex);
+
+        /* use <= b/c if arr only contains 2 elements, it will not be checked */
+        while(lowest <= highest) {
+            // if the target string is found
+            if(target.compareTo(middleString) == 0) {
+                return middleIndex;
+            }
+            // if target string is greater than middle string
+            if(target.compareTo(middleString) > 0) {
+                // move lowest to right of middle and continue searching
+                lowest = middleIndex + 1;
+                middleIndex = getMiddleIndex(lowest, highest);
+                middleString = months.get(middleIndex);
+            }
+            // if target string is less than middle string
+            if(target.compareTo(middleString) < 0) {
+                // move highest to left of middle and continue searching
+                highest = middleIndex - 1;
+                middleIndex = getMiddleIndex(lowest, highest);
+                middleString = months.get(middleIndex);
+            }
+        }
+        // did not find the target
+        return -1;
     }
 
+    // random filling (can be filled in any way)
     public static void fillMonths(ArrayList<String> months) {
         months.add("August");
         months.add("October");
@@ -24,6 +53,22 @@ public class MonthsHelper {
     }
 
     public static void sortMonths(ArrayList<String> months) {
+        for(int i = 0; i < months.size(); i++) {
+            for(int j = i + 1; j < months.size(); j++) {
+                // if current string is bigger than next string
+                if(months.get(i).compareTo(months.get(j)) > 0) {
+                    // set temporary string as current string
+                    String temp = months.get(i);
+                    // change current string into next string: 0, 1 -> 1,1
+                    months.set(i, months.get(j));
+                    // change next string into temp (old current string): 1,1 -> 1,0
+                    months.set(j, temp);
+                }
+            }
+        }
+    }
 
+    private static int getMiddleIndex(int one, int two) {
+        return (one + two) / 2;
     }
 }
--- src/main/java/org/APCSA2023/BinarySearch/Tester.java
+++ src/main/java/org/APCSA2023/BinarySearch/Tester.java
@@ -1,18 +1,39 @@
+// Jiale Yu
+// 3/22/24
+// AP Comp Sci Binary Search
+
 package org.APCSA2023.BinarySearch;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
         ArrayList<String> months = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        String inputMonth;
 
         MonthsHelper.fillMonths(months);
 
         MonthsHelper.sortMonths(months);
 
+        System.out.println("---------------Sorted Months---------------");
         for(String i: months) {
             System.out.println(i);
         }
+        System.out.println("-------------------------------------------");
 
+        System.out.print("Enter month: ");
+        inputMonth = input.next();
+
+        System.out.println("Searching for " + inputMonth + "...");
+        int index = MonthsHelper.monthBinarySearch(months, inputMonth);
+
+        if(index != -1) {
+            System.out.println(inputMonth + " was found at index: " + index);
+        }
+        else {
+            System.out.println(inputMonth + " was not found: " + index);
+        }
     }
 }
